a51288889c143b75436a97176f48ffd3d74a9520
Bruno Fitas <brunofitas@gmail.com>
2018-02-04 23:56:30 +0000

    Improved entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,17 +10,17 @@ echo syncLimit=${SYNC_LIMT:-5}              >> ${ZOO_CFG}
 echo dataDir=${DATA_DIR:-/var/zookeeper}    >> ${ZOO_CFG}
 echo clientPort=${CLIENT_PORT:-2181}        >> ${ZOO_CFG}
 
-if [[ ! -z "${MAX_CLIENT_CONXNS}" ]]; then
+if [[ -n ${MAX_CLIENT_CONXNS} ]]; then
     echo "maxClientCnxns=${MAX_CLIENT_CONXNS:-60}" >> ${ZOO_CFG}
 fi
 
 
-if [[ ! -z "${AUTO_PURGE__SNAP_RETAIN_COUNT}" ]]; then
+if [[ -n ${AUTO_PURGE__SNAP_RETAIN_COUNT} ]]; then
     echo "autopurge.snapRetainCount=${AUTO_PURGE__SNAP_RETAIN_COUNT:-3}" >> ${ZOO_CFG}
 fi
 
 
-if [[ ! -z "${AUTO_PURGE__PURGE_INTERVAL}" ]]; then
+if [[ -n ${AUTO_PURGE__PURGE_INTERVAL} ]]; then
     echo "autopurge.purgeInterval=${AUTO_PURGE__PURGE_INTERVAL:-1}" >> ${ZOO_CFG}
 fi
 
